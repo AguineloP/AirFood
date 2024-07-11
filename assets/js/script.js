da199929e6ec156955fa4a0cb653d9064fb3e37c
Aguinelo Pimenta B. Neto <167259858+AguineloP@users.noreply.github.com>
2024-07-11 13:57:25 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
+import { GLTFLoader } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
+
+
 document.addEventListener('DOMContentLoaded', (event) => {
     const ptButton = document.getElementById('ptButton');
     const enButton = document.getElementById('enButton');
@@ -27,4 +31,35 @@ document.addEventListener('DOMContentLoaded', (event) => {
     } else {
         switchLanguage('pt');
     }
+    
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / 500, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, 500);
+    document.getElementById('3d-container').appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    const animate = function () {
+        requestAnimationFrame(animate);
+
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+    };
+
+    animate();
+
+        const loader = new GLTFLoader();
+loader.load('https://raw.githubusercontent.com/AguineloP/AirFood/main/assets/model/modelo3d_drone.glb', function (gltf) {
+        scene.add(gltf.scene);
+    }, undefined, function (error) {
+        console.error(error);
+    });
 });
